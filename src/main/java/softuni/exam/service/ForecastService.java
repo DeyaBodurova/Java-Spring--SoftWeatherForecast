51c93aee0bf9d09259d16963fee499b4e8b2a239
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-12-02 17:48:23 +0200

    wip

--- src/main/java/softuni/exam/service/ForecastService.java
+++ src/main/java/softuni/exam/service/ForecastService.java
@@ -1,16 +1,20 @@
 package softuni.exam.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import softuni.exam.repository.ForecastRepository;
+
 import javax.xml.bind.JAXBException;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
-// TODO: Implement all methods
 public interface ForecastService {
 
     boolean areImported();
 
-    String readForecastsFromFile() throws IOException;
-	
-	String importForecasts() throws IOException, JAXBException;
+    String readForecastsFromFile() throws IOException, JAXBException;
 
-    String exportForecasts();
+    String importForecasts() throws IOException, JAXBException;
+
+    String exportForecasts() throws JAXBException, FileNotFoundException;
 }
