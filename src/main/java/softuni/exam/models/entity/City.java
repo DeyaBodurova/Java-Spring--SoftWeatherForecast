583f3c12130d4a5dd731de7f8efac3be1d52baf4
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-12-02 16:25:42 +0200

    wip

---
+++ src/main/java/softuni/exam/models/entity/City.java
@@ -0,0 +1,34 @@
+package softuni.exam.models.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "cities")
+public class City extends BaseEntity {
+
+    @Column(name = "city_name", unique = true, nullable = false)
+    @Size(min = 2, max = 60)
+    private String cityName;
+
+    @Column(columnDefinition = "TEXT")
+    @Size(min = 2)
+    private String description;
+
+    @Column(nullable = false)
+    @Size(min = 500)
+    private Long population;
+
+    @ManyToOne
+    private Country countries;
+}
